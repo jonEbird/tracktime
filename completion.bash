814ef1b2e932ebe43f940771172a197c3c5dd124
jonEbird <jonEbird@49b52797-1f53-0410-8bed-b3af8e18f4a7>
2009-07-07 23:40:12 +0000

    Finally cleaned up bash completion. Now supports subdir completion which is useful with the -w option

--- completion.bash
+++ completion.bash
@@ -18,34 +18,27 @@ if [ "${dirilivein}/tasks" != "$TASKSDIR" ]; then
 fi
 
 # Let's update your PATH considering you'll be using this.
-echo ":${PATH}:" | grep -q ":${dirilivein}:"
-if [ $? -ne 0 ]; then
+if ! echo ":${PATH}:" | grep -q ":${dirilivein}:"; then
     PATH="${PATH}:${dirilivein}"
 fi
 
 ttcomplete() { 
     #echo "1: \"$1\" 2: \"$2\" 3: \"$3\""
     # 2=current_typing_arg 3=previous_arg
-    if [ "$1" == "$3" ]; then
-	# nothing typed yet
-	potentials=$(/bin/ls -ld ${TASKSDIR}/${2}* 2>&- | awk -F/ '/^d/{ print $NF }' | xargs)
-	#printf " %s" $potentials
-	COMPREPLY=(${potentials})
-    elif [ "$3" == "-d" ]; then
-	#potentials=$(sed -n 's/^ *formats *= *(\([^)]*\)) *$/\1/p' ${dirilivein}/tt.py | sed 's/, / /g')
-	#COMPREPLY=(${potentials})
+    COMPREPLY=()
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    if [ "${prev}" == "-d" ]; then
+	#echo "Case #1"
 	COMPREPLY=('%Y-%m-%d %H:%M:%S' '%Y-%m-%d' '%Y-%m-%d %H:%M' '%m/%d' '%m/%d/%y' '%m/%d/%Y' '%m-%d-%y' '%m-%d-%Y' '%H:%M' '%M:%M:%S')
     elif [ -d "${TASKSDIR}/$3" ]; then
-	#echo "1: \"$1\" 2: \"$2\" 3: \"$3\""
-	# we are on minutes arg
+	#echo "Case #2"	# we are on minutes arg
 	COMPREPLY=(30 60 90 120)
     else
-        # okay, nothing special...
-        #potentials=$(/bin/ls -d ${2}* 2>&- | xargs)
-        eval files=$(/bin/echo ${2}* 2>&-)
-        #potentials=$(for f in ${2}*; do if [ -d $f ]; then echo $f/; else echo $f; fi; done)
-	potentials=$(for f in ${files}; do if [ -d $f ]; then echo $f/; else echo $f; fi; done)
-        COMPREPLY=(${potentials})
+	#echo "Case #3"
+	#COMPREPLY=( $(compgen -f "${TASKSDIR}/${cur}") )
+	COMPREPLY=( $(compgen -f $TASKSDIR/${cur} | sed "s|^${TASKSDIR}/||g") )
     fi
 }
 
